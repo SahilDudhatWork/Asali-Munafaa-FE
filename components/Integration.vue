99ceb97a63e0c005befd54a8e7fe7bd9e3bddfd2
SahilDudhatWork <sahildudhat03@gmail.com>
2024-07-15 16:46:54 +0530

    removed shopify logo

--- components/Integration.vue
+++ components/Integration.vue
@@ -12,7 +12,7 @@
       <div
         class="flex justify-center flex-wrap xl:gap-[110px] md:gap-[70px] gap-[40px]"
       >
-        <img src="../static/Images/logo1.webp" />
+        <!-- <img src="../static/Images/logo1.webp" /> -->
         <img src="../static/Images/logo2.webp" />
         <img src="../static/Images/logo3.webp" />
         <img src="../static/Images/logo4.webp" />
